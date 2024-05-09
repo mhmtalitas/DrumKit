f4e42fd9a65fa9e988d5c8786ef99eeeaebffa8b
mehmtalitas <airyus99@gmail.com>
2024-05-09 18:48:04 +0300

    first commit

---
+++ scrpit.js
@@ -0,0 +1,56 @@
+
+function playSoundA() {
+
+    const audio = document.querySelector("#buttonA")
+    audio.play();
+
+}
+
+function playSoundS() {
+
+    const audio = document.querySelector("#buttonS")
+    audio.play();
+
+}
+function playSoundD() {
+
+    const audio = document.querySelector("#buttonD")
+    audio.play();
+
+}
+function playSoundF() {
+
+    const audio = document.querySelector("#buttonF")
+    audio.play();
+
+}
+function playSoundG() {
+
+    const audio = document.querySelector("#buttonG")
+    audio.play();
+
+}
+function playSoundH() {
+
+    const audio = document.querySelector("#buttonH")
+    audio.play();
+
+}
+function playSoundJ() {
+
+    const audio = document.querySelector("#buttonJ")
+    audio.play();
+
+}
+function playSoundK() {
+
+    const audio = document.querySelector("#buttonK")
+    audio.play();
+
+}
+function playSoundL() {
+
+    const audio = document.querySelector("#buttonL")
+    audio.play();
+
+}
